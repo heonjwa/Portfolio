44fbf338ea1900b3ef907e827ef3469efe6394a9
heonjwa <heonjwa@gmail.com>
2023-12-18 21:21:08 -0800

    Started next js project

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Heon Jwa | Portfolio',
+  description: 'Heon is a software engineer at the University of Washington',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-gray-50`}>{children}
+      </body>
+    </html>
+  )
+}
