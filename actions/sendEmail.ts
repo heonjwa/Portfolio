e466e1195b48ec86ffce845552c8710b24d92852
heonjwa <heonjwa@gmail.com>
2023-12-25 00:44:04 -0800

    resend key

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,7 +5,7 @@ import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/email/contact-form-email";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_bx4DL9Xf_J1RcuLh9qryjyT4PaFKaeNA1');
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
